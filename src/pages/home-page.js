0dec7127444421f10b117d5ed552cc57d0ceb21e
Jackson Currie <j.currie1999@gmail.com>
2020-10-26 22:47:30 +1300

    Add color styling to multiple cards

--- src/pages/home-page.js
+++ src/pages/home-page.js
@@ -9,16 +9,14 @@ function HomePage() {
       <Header />
 
       <main>
-        <Card
-          imageSrc="https://d1csarkz8obe9u.cloudfront.net/posterpreviews/action-thriller-book-cover-design-template-3675ae3e3ac7ee095fc793ab61b812cc.jpg?ts=1588152105"
-          title="The king of drugs"
-          author="Nora Barrett"
-        />
-        <Card
-          imageSrc="https://d1csarkz8obe9u.cloudfront.net/posterpreviews/action-thriller-book-cover-design-template-3675ae3e3ac7ee095fc793ab61b812cc.jpg?ts=1588152105"
-          title="The king of drugs"
-          author="Nora Barrett"
-        />
+        {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map((i) => (
+          <Card
+            key={i}
+            imageSrc="https://d1csarkz8obe9u.cloudfront.net/posterpreviews/action-thriller-book-cover-design-template-3675ae3e3ac7ee095fc793ab61b812cc.jpg?ts=1588152105"
+            title="The king of drugs"
+            author="Nora Barrett"
+          />
+        ))}
       </main>
 
       <Footer />
